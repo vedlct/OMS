03d8fedddc8871e96cfa864373514ad73bd61942
SknightS <md.sakibrahman@gmail.com>
2017-07-13 10:38:44 +0600

    sakib

---
+++ app/registrationm.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Registration extends Model
+{
+    //
+    protected $table = 'customer_info';
+    public function insertdata(){
+
+
+    }
+}
